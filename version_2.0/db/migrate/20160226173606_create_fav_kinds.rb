86dbb2d3f7e64193e20d5f0ceec0c09c0eae6565
CC Wang <chongchong1110@gmail.com>
2016-03-02 14:58:59 -0800

    add movie_id in fav_kinds table

--- version_2.0/db/migrate/20160226173606_create_fav_kinds.rb
+++ version_2.0/db/migrate/20160226173606_create_fav_kinds.rb
@@ -3,7 +3,7 @@ class CreateFavKinds < ActiveRecord::Migration
     create_table :fav_kinds do |t|
       t.references :user, index: true
       t.integer :genre_id
-
+      t.integer :movie_id
       t.timestamps
     end
   end
